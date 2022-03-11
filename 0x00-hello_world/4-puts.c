212e1d82fb6b29608fd8e92ac1dec2e89204fd21
Ekene <clarencekene@outlook.com>
2022-03-11 04:42:18 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	char str[55];
+	char str[53];
 
 	strcpy(str, "\"Programming is like building a multilingual puzzle\n");
 
