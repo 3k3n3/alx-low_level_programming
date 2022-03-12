5ab20d25ff66f8d63542026a01566e1b74ecab0b
Ekene <clarencekene@outlook.com>
2022-03-12 04:02:28 -0800

    task 100 2nd attempt

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ * main entrance'
+ *
+ * Retutn 1 (Success)
+ */
+int main(void)
+{
+	printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
