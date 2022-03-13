530fa4860fb031964701df18a756dc3530714298
Ekene <clarencekene@outlook.com>
2022-03-13 06:46:25 -0700

    task 5-8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char h;
+
+	for (h = '0x0' ; h <= '0xf' ; h++)
+	{
+		putchar(h);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
