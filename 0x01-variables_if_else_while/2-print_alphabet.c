e3754e640f500caad39f6cba18442ca6c6ceb30d
Ekene <clarencekene@outlook.com>
2022-03-13 00:45:41 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Reurn: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c ='a' ; c <= 'z' ; ++c)
+	{
+		putchar(c);
+		"\n";
+	}
+	return (0);
+}
