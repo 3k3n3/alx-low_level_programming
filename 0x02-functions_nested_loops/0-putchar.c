8e09a6536305cbee2ebe7a4a2a60f93b658cb236
Ekene <clarencekene@outlook.com>
2022-03-15 12:01:54 -0700

    tasks 0-2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - the program should return 0
+ *
+ * Return: 0 success!
+ */
+int main(void)
+{
+	
+	putchar(95);
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * print the alphabet in lower case 10 times
+ * Return: Always 0
+ */
+int main(void)
+{
+	int alphabet;
+	int times;
+	
+	for (times = 1 ; times <= 10 ; times++)
+	{
+		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+		{
+			putchar(alphabet);
+		}
+		putchar('\n');
+	}
+	
+	return (0);
+}	
