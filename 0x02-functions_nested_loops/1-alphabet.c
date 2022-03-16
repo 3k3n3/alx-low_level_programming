a3af153e18339b645d7751d372233bbb9fa7fd62
Ekene <clarencekene@outlook.com>
2022-03-15 21:25:59 -0700

    fresh

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,21 +3,15 @@
 /**
  * main - check the code
  * print the alphabet in lower case
- * Return: Always 0
+ * Return: void
  */
 void print_alphabet(void)
 {
-	int alpha;
+	char alpha;
 
 	for (alpha = 'a' ; alpha <= 'z' ; alpha ++)
 	{
 		_putchar(alpha);
 	}
 	_putchar('\n');
-}
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }	
