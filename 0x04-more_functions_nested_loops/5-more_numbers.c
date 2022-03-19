06064d1f9669100bf67c38d73437b7dd91f07a45
Ekene <clarencekene@outlook.com>
2022-03-18 17:11:48 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers: print from 0 to 14
+ * more_numbers - print from 0 to 14
  *
  * Return: 0
  */
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int i, a;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		for (a = 0 ; a <= 15 ; a++)
+		for (a = 0 ; a <= 14 ; a++)
 		{
 			if (a >= 10)
 			{
