bab8b20e65a8ded71fd38d60c0a16816b92ff274
Ekene <clarencekene@outlook.com>
2022-03-18 17:40:16 -0700

    task 6"

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - uses '_' to print lines
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0 ; c <= n , c++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
