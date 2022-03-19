f8f05804a4146a6fba74ab8ed1aecdbf43db0801
Ekene <clarencekene@outlook.com>
2022-03-18 19:17:48 -0700

    9 final

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - famous fizz buzz in c
  * 
  * Return: Always(0)
  */
-
 int main(void)
 {
 	int i;
@@ -12,22 +12,15 @@ int main(void)
 	for (i = 1; i <= 99; i++)
 	{
 		if (i % 15 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%i ", i);
-		}
 	}
 	printf("Buzz\n");
+
 	return (0);
 }
