1fe8a2ff51ada31f5197e47aff28a446e826d4b5
Ekene <clarencekene@outlook.com>
2022-03-21 13:45:11 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*str);
+		s--;
+	}
+	_putchar('\n');
+}
