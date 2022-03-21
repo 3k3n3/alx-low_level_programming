fd2723ff6ee8093cd0ce41b0e7950be0e483673a
Ekene <clarencekene@outlook.com>
2022-03-21 15:05:40 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,31 @@
  * Return: void
  */
 void print_rev(char *s)
-{
-	char *r = s;
-	while (*(r + 1) != '\0')
-		r++;
-	while (r > s)
+{	
+	int length, c;
+	char *begin, *end, temp;
+
+	length = string_length(s);
+	begin = s;
+	end = s;
+
+	for (c = 0; c < length - 1; c++)
+		end++;
+	for (c = 0; c < length / 2; c++)
 	{
-		char tmp = *r;
-		*r-- = *s;
-		*s++ = tmp;
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+
+		begin++;
+		end--;
 	}
-	_putchar('\n');
+}
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+		c++;
+	return c;
 }
