17cea52c87b643153e8d88e5e99bbf2ef872db4e
Ekene <clarencekene@outlook.com>
2022-03-30 21:09:30 -0700

    puts string with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s[0] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	return (*s) + _puts_recursion(s + 1);
+	return (*s)
+		_puts_recursion(s + 1);
 }
