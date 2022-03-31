3096bd7b4fd1167b3b9058edb380a1c37bb8859d
Ekene <clarencekene@outlook.com>
2022-03-30 21:43:22 -0700

    reverse string with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of str
+ * @s: str
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
