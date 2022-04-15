219b7a99fad93eb918b38827439198ffdf8e901a
Ekene <clarencekene@outlook.com>
2022-04-15 12:21:11 -0700

    task 3 ma pa mi now

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,13 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * get_op_func - function pointer that selects the correct function to perform
- * the operation asked by the user
- * @s: the operator given by the user
- *
- * Return: pointer to the function that corresponds to the
- * operator given as a parameter
+ * get_op_func - checks the element and initializes the op addresses
+ * @s: pointer to string
+ * Return: Null if false
  */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -18,16 +18,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
-
-	i = 0;
-
-	while (ops[i].op)
+	int i = 0;
+	/* go through struct array and check operator*/
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
+		if (strcmp(ops[i].op, s) == 0)/*compare two pointers*/
+			return (ops[i].f);/*returning result of the function*/
 		i++;
 	}
-
 	return (NULL);
 }
