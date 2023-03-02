2e74a59ec0368755a026df60d00a943aa79b75df
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-02 22:42:09 +0000

    Correction for Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,5 +10,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	/*return strcmp(s1, s2);*/
+	int i;
+	for (i = 0; s1[i] != '\0' && s2[1] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
 	return (s1[0] - s2[0]);
 }
