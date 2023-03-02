ba5f18e92c264210723f081f07d35137e12f7216
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-02 22:31:06 +0000

    Tasks 0 - 3, README & main.h

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	/*return strcmp(s1, s2);*/
+	return (s1[0] - s2[0]);
+}
