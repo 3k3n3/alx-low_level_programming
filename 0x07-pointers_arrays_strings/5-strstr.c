80430bb4ed3db0d05091fb9d3d5e30e389030a54
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-07 22:00:05 +0000

    TAsks 7 & 8

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,17 +9,17 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	if (*needle == '0')
-		return (haystack);
-	for (; haystack != 0; haystack++)
-	{
-		char *h = haystack;
-		char *n = needle;
+	int i, j;
 
-		for (; *n != '\0' && *h == *n; h++, n++)
-			;
-		if (*n == '\0')
-			return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
 	}
 	return (NULL);
 }
