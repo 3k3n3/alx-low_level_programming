e12ecbe7cfb41c5b91996f91dea64b3f6249f893
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-09 04:48:28 +0000

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prime number?
+ * @n: number
+ *
+ * Return: 1 if prime number else 0
+ */
+int is_prime_number(int n)
+{
+	if (n < 0)
+		n *= -1;
+	if (n < 2)
+		return (0);
+	if (n == 2 || n == 3)
+		return (1);
+	if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+	return (prime_number(n , 3));
+}
+
+/**
+ * prime_number - helper function
+ * @n: number
+ * @i: iterable
+ *
+ * Return: 0 or 1
+ */
+int prime_number(int n, int i)
+{
+	if (n % i == 0)
+		return (0);
+	if (i * i > n)
+		return (1);
+	return prime_number(n, i + 2);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int first_sqrt_recursion(int n, int i);
 int is_prime_number(int n);
+int prime_number(int n, int i);
