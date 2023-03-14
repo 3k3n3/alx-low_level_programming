a3fdaf28e4849b14316840092f7d2feca0092c0e
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-14 19:43:01 +0000

    Tasks 0, 1, main.h & readme file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0, success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
