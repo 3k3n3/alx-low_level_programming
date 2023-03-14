f9fd060e272f04d4bd427867b2c9a4e106b78199
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-14 21:06:58 +0000

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0, success
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) > 0)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
