894e13bc7aaed0288b1b81b6d17ec83022d44785
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-15 06:15:24 +0000

    TAsk 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0, success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		if (atoi(argv[1]) < 0)
+			printf("0\n");
+		else
+		{
+			if argv
+		}	
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
