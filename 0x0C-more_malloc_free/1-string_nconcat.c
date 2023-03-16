8f2802697cb7f87667168f2a20ccdaa0f53c1ef5
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-16 21:13:11 +0000

    Task 1 Betty corrections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * string_noncat - concatenates 2 strings
+ * string_nconcat - concatenates 2 strings
  * @s1: string 1
  * @s2: string 2
- * @n number oof bytes
+ * @n: number of bytes
  *
  * Return: NULL or pointer
  */
@@ -23,28 +23,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s2 != NULL)
 	{
-		for (j = 0; s2[j] != '\0'; j++);
+		for (j = 0; s2[j] != '\0'; j++)
+			;
 	}
 	else
 		j = 0;
-	
+
 	l = j > n ? n : j;
 
 	a = malloc((i + l + 1) * sizeof(*a));
 	if (a == NULL)
 		return (NULL);
-	
+
 	for (x = 0; x < i; x++)
 		a[x] = s1[x];
-	
+
 	for (y = 0; y < l; x++, y++)
 		a[x] = s2[y];
 
 	a[x] = '\0';
-
-
-
-
 
 	return (a);
 }
