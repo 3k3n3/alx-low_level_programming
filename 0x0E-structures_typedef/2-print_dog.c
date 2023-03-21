0bcd0777246e0f435bf6282d2f094911bc97cae8
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-21 00:11:34 +0000

    Task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints struct dog
+ * @d: pointer to struct dog
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
+	printf("Name: %s\n", d->name ? d->name : "nil");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "nil");
+}
