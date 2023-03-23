bdf04aa5f6a0f34e1e9df15dc579421f3aa02d32
3k3n3@vagrant <clarencekene@outlook.com>
2023-03-23 19:25:23 +0000

    Task 3 files and updated function_pointers.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: value of operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: value of operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: value of operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: value of operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: value of operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
