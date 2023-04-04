44378ec8c7e820d434ca2520a2a372652473a0a2
3k3n3@vagrant <clarencekene@outlook.com>
2023-04-04 17:14:38 +0000

    Correction 7: Task 9, checker error?

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,8 +14,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *temp, *current;
 
 	new_node = malloc(sizeof(listint_t));
-	if (*head == NULL)
-		return (NULL);
 
 	if (new_node == NULL)
 		return (NULL);
@@ -43,7 +41,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-	free(new_node);
 
 	return (NULL);
 }
