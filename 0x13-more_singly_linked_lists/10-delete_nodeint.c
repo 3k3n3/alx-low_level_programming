2e652a04a1ec5c3d54e72ac62b0b850dc39cb6de
3k3n3@vagrant <clarencekene@outlook.com>
2023-04-04 17:27:57 +0000

    Correction 4: Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current, *temp;
 
 	current = *head;
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	if (current == NULL)
@@ -34,10 +34,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			temp = current->next;
 			current->next = temp->next;
 			free(temp);
+			return (1);
 		}
 		current = current->next;
 		i++;
 	}
 
-	return (1);
+	return (-1);
 }
