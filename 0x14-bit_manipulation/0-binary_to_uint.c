a3db17382cdedf61fd51c4ec9b1234478036cca7
3k3n3@vagrant <clarencekene@outlook.com>
2023-04-08 19:29:33 +0000

    README, header and Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: array of char containing bits
+ *
+ * Return: converted decimal or 0 if not valid
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, j, temp;
+	unsigned int add = 0;
+	unsigned int mul = 1;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	i -= 1;
+
+	for (j = 0; b[j] != '\0'; j++)
+	{
+		if (b[j] == '1')
+		{
+			temp = i - 1;
+			for (; i > 0; i--)
+				mul *= 2;
+			i = temp;
+			add += mul;
+			mul = 1;
+		}
+
+		if (b[j] == '0')
+		{
+			temp = i - 1;
+			for (; i > 0; i--)
+				mul *= 0;
+			i = temp;
+			add += mul;
+			mul = 1;
+		}
+	}
+
+	if (b[j - 1] == '1')
+		return (add);
+	else
+		return (add - 1);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,12 @@
+## 0x14. C - Bit manipulation
+
+ A | B | A|B | A&B | A^B | ~A
+ --- | --- | --- | --- | --- | ---
+ 0 | 0 | 0 | 0 | 0 | 1
+ 0 | 1 | 1 | 0 | 1 | 1
+ 1 | 0 | 1 | 0 | 1 | 0
+ 1 | 1 | 1 | 1 | 0 | 0
+
+Useful Resource:
+- [Bitwise Operators](https://www.programiz.com/c-programming/bitwise-operators)
+- [Converting between number bases](https://www.circuitbread.com/tutorials/binary-hexadecimal-and-other-base-numbers)
