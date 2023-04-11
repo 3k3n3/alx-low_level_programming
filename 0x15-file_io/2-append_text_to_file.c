657eff9b68dc26ca65de92cdd42862cb2a6bb59d
3k3n3@vagrant <clarencekene@outlook.com>
2023-04-11 04:58:36 +0000

    Corrections: Task 2 - Append text to end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,16 +20,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
 
-	if (text_content != NULL)
 		bytes_w = write(fd, text_content, i);
 
-	if (bytes_w == -1)
-	{
-		close(fd);
-		return (-1);
+		if (bytes_w == -1)
+			return (-1);
 	}
 
 	close(fd);
