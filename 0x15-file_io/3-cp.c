5411740ffcb15731b15113266431de7679209bd3
3k3n3@vagrant <clarencekene@outlook.com>
2023-04-11 01:35:08 +0000

    Tast 3: program that copies the content of a file to another file; updated main.h

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+void print_error(const char *message, int exit_code)
+{
+	dprintf(STDERR_FILENO, "Error: %s\n", message);
+	exit(exit_code);
+}
+
+int main(int ac, char **av)
+{
+	char *file_from = av[1];
+	char *file_to = av[2];
+
+	int fd_from = open(file_from, O_RDONLY);
+	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+	char buf[BUF_SIZE];
+	int bytes_read, bytes_written;
+
+	if (ac != 3)
+		print_error("Usage: cp file_from file_to", 97);
+
+	if (fd_from == -1)
+		print_error(strerror(errno), 98);
+
+	if (fd_to == -1)
+		print_error(strerror(errno), 99);
+
+	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buf, bytes_read);
+		if (bytes_written != bytes_read)
+			print_error(strerror(errno), 99);
+	}
+	
+	if (bytes_read == -1)
+		print_error(strerror(errno), 98);
+
+	if (close(fd_from) == -1)
+		print_error(strerror(errno), 100);
+
+	if (close(fd_to) == -1)
+		print_error(strerror(errno), 100);
+
+	return (0);
+}
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUF_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
