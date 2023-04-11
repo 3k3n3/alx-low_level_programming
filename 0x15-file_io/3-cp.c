4b047c03bcfc5378b0861e0cb30e366c2077c21d
3k3n3@vagrant <clarencekene@outlook.com>
2023-04-11 01:41:25 +0000

    Tast 3: program that copies the content of a file to another file; updated main.h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,18 +1,33 @@
 #include "main.h"
 
+/**
+ * print_error - prints error
+ * @message: message
+ * @exit_code: exit code
+ *
+ * Return: void
+ */
 void print_error(const char *message, int exit_code)
 {
 	dprintf(STDERR_FILENO, "Error: %s\n", message);
 	exit(exit_code);
 }
 
+/**
+ * main - Entry point, copies one file to another
+ * @ac: args count
+ * @av: args vector
+ *
+ * Return: Always 0, success
+ */
 int main(int ac, char **av)
 {
 	char *file_from = av[1];
 	char *file_to = av[2];
 
 	int fd_from = open(file_from, O_RDONLY);
-	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
 	char buf[BUF_SIZE];
 	int bytes_read, bytes_written;
@@ -32,7 +47,7 @@ int main(int ac, char **av)
 		if (bytes_written != bytes_read)
 			print_error(strerror(errno), 99);
 	}
-	
+
 	if (bytes_read == -1)
 		print_error(strerror(errno), 98);
 
